7e3b6e48bd3f84d61824b4e7881247469c7a5064
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-26 19:09:33 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/Grid.php
+++ src/Osynapsy/Bcl4/Grid.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4;
 
 use Osynapsy\Html\Component;
@@ -7,7 +17,7 @@ use Osynapsy\Html\Tag;
 /**
  * Description of CardGrid
  *
- * @author Pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class Grid extends Component
 {
