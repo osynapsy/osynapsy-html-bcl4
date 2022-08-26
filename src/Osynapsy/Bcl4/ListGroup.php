44fea0dc9df13faa41933714e3ccae406d4db552
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-26 19:07:56 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/ListGroup.php
+++ src/Osynapsy/Bcl4/ListGroup.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4;
 
 use Osynapsy\Html\Component;
@@ -6,7 +16,7 @@ use Osynapsy\Html\Component;
 /**
  * Description of ListGroup
  *
- * @author Pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class ListGroup extends Component
 {
