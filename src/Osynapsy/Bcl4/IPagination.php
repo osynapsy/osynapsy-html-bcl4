0e43026e1b4acb8d343c2c3197f74de63d5a3063
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-26 19:09:25 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/IPagination.php
+++ src/Osynapsy/Bcl4/IPagination.php
@@ -1,10 +1,20 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4;
 
 /**
  * Description of IPagination
  *
- * @author Pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 interface IPagination
 {
