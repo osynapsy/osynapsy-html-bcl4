8980e61c65cacc48d17eac9bfbb92d4a7d06b52f
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-26 19:09:57 +0200

    Added license disclaimer

--- src/Osynapsy/Bcl4/Dropdown.php
+++ src/Osynapsy/Bcl4/Dropdown.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Bcl4;
 
 use Osynapsy\Html\Tag;
@@ -7,7 +17,7 @@ use Osynapsy\Html\Component;
 /**
  * Description of Dropdown
  *
- * @author Pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class Dropdown extends Component
 {
