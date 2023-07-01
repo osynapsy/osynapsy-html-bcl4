7fc209afd0361bbce4bb6a7bc7043b43262f9590
Pietro Celeste <p.celeste@osynapsy.net>
2023-07-01 13:42:45 +0200

    Bugfix

--- src/Osynapsy/Bcl4/Switcher.php
+++ src/Osynapsy/Bcl4/Switcher.php
@@ -13,7 +13,7 @@ namespace Osynapsy\Bcl4;
 
 use Osynapsy\Html\Tag;
 use Osynapsy\Html\Component\AbstractComponent;
-use Osynapsy\Html\Component\CheckBox;
+use Osynapsy\Html\Component\InputCheckBox;
 
 /**
  * Description of Switcher
@@ -29,25 +29,28 @@ class Switcher extends AbstractComponent
     {
         parent::__construct('div', $id.'_container');
         $this->addClass('custom-control custom-switch');
-        $this->checkBox = $this->add(new CheckBox($id))->getCheckBox();
-        $this->checkBox->addClass('custom-control-input');
-        $this->label = $this->add(new Tag('label', null, "custom-control-label"))->attribute('for', $id);
-        $this->label->add($label);
+        $this->checkBox = $this->add($this->inputCheckFactory($id));
+        $this->label = $this->add($this->labelFactory($label, $id));
+    }
+
+    protected function inputCheckFactory($id)
+    {
+        $checkBox = new InputCheckBox($id);
+        $checkBox->addClass('custom-control-input');
+        return $checkBox;
+    }
+
+    protected function labelFactory($label, $id)
+    {
+        $Label = new Tag('label', null, "custom-control-label");
+        $Label->attribute('for', $id);
+        $Label->add($label);
+        return $Label;
     }
 
     public function getCheckBox()
     {
         return $this->checkBox;
-    }
-
-    public function setAction($action, $parameters = null, $class = 'click-execute', $confirmMessage = null)
-    {
-        $this->checkBox->attributes(['data-action' => $action, 'data-action-parameters' => $parameters]);
-        $this->checkBox->addClass($class);
-        if (!empty($confirmMessage)) {
-            $this->checkBox->attribute('data-action-confirm', $confirmMessage);
-        }
-        return $this;
     }
 
     public function disable()
