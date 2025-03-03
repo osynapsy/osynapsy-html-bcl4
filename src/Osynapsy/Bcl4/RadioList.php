d529a80bc50d99b06cfb06623eef39b811860319
Pietro Celeste <p.celeste@osynapsy.net>
2025-03-03 12:20:20 +0100

    Added new RadioList component

---
+++ src/Osynapsy/Bcl4/RadioList.php
@@ -0,0 +1,37 @@
+<?php
+namespace Osynapsy\Bcl4;
+
+use Osynapsy\Html\Component\AbstractComponent;
+use Osynapsy\Html\Component\RadioBox;
+
+/**
+ * Description of RadioList
+ *
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
+ */
+class RadioList extends AbstractComponent
+{
+    public function __construct($id = null)
+    {
+        parent::__construct('div', $id);
+    }
+
+    public function preBuild()
+    {
+        $request = $_REQUEST[$this->id] ?? null;
+        foreach($this->dataset as $i => $row) {
+            $v = is_array($row) ? array_values($row) : [$i, $row];
+            $this->add($this->radioBoxFactory($i, $v[0], $v[1], $request));
+        }
+    }
+
+    protected function radioBoxFactory($idx, $value, $label, $request)
+    {
+        $RadioBox = new RadioBox($this->id . '_' . $idx, $label, $value);
+        $RadioBox->attribute('name', $this->id);
+        if (!is_null($request) && $request == $value) {
+            $RadioBox->setChecked(true);
+        }
+        return $RadioBox;
+    }
+}
