77250e1a7773aaa1f194b374e13afe6e9df35fdc
Pietro Celeste <p.celeste@osynapsy.net>
2025-03-11 17:03:45 +0100

    Bugfix

--- src/Osynapsy/Bcl4/DateBox.php
+++ src/Osynapsy/Bcl4/DateBox.php
@@ -4,14 +4,15 @@ namespace Osynapsy\Bcl4;
 use Osynapsy\Html\Component\InputDate;
 
 /**
- * Description of DatePicker
+ * Description of DateBox
  *
- * @author pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class DateBox extends InputDate
 {
     public function __construct($name, $class = '')
     {
         parent::__construct($name, trim('form-control '.$class));
+        $this->attribute('type', 'date');
     }
 }
--- src/Osynapsy/Bcl4/DateTimeBox.php
+++ src/Osynapsy/Bcl4/DateTimeBox.php
@@ -6,12 +6,13 @@ use Osynapsy\Html\Component\InputDateTime;
 /**
  * Description of DatePicker
  *
- * @author pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class DateTimeBox extends InputDateTime
 {
     public function __construct($name, $class = '')
     {
         parent::__construct($name, trim('form-control '.$class));
+        $this->attribute('type', 'datetime-local');
     }
 }
