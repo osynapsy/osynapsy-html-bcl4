77629ae9bae8bda445778705ae874c48dd9d65f0
Pietro Celeste <p.celeste@osynapsy.net>
2025-09-27 18:43:16 +0200

    Improve code

--- assets/bcl4/gallery/script.js
+++ assets/bcl4/gallery/script.js
@@ -1,26 +1,50 @@
-Bcl4Gallery = 
-{
-    init : function()
-    {        
-        $(document).on('click', '.osy-bcl4-gallery .osy-bcl4-gallery-thumbnail', function(e) {
-            let thumbnailElement = e.target;            
-            let galleryElement = $(thumbnailElement).closest('div.osy-bcl4-gallery');
-            let fieldValues = $(thumbnailElement).data('fields');            
-            $('.osy-bcl4-gallery-viewer', galleryElement).attr('src', $(thumbnailElement).attr('src')); 
-            $('.osy-bcl4-gallery-image-delete', galleryElement).attr('data-action-parameters', $(thumbnailElement).data('id')); 
-            $('.osy-bcl4-gallery-image-description').attr('data-action-parameters', $(thumbnailElement).data('id')); 
-            $('.osy-bcl4-gallery-image-id').val($(thumbnailElement).data('id'));            
+const Bcl4Gallery = {
+    init: function () {
+        document.addEventListener('click', function (e) {
+            if (!e.target.classList.contains('osy-bcl4-gallery-thumbnail')) {
+                return;
+            }
+
+            let thumbnailElement = e.target;
+            let galleryElement = thumbnailElement.closest('div.osy-bcl4-gallery');
+            let fieldValues = thumbnailElement.dataset.fields ? JSON.parse(thumbnailElement.dataset.fields) : null;
+
+            // Viewer image
+            let viewer = galleryElement.querySelector('.osy-bcl4-gallery-viewer');
+            if (viewer) {
+                viewer.setAttribute('src', thumbnailElement.getAttribute('src'));
+            }
+
+            // Delete button
+            let deleteBtn = galleryElement.querySelector('.osy-bcl4-gallery-image-delete');
+            if (deleteBtn) {
+                deleteBtn.setAttribute('data-action-parameters', thumbnailElement.dataset.id);
+            }
+
+            // Description button
+            let descBtn = galleryElement.querySelector('.osy-bcl4-gallery-image-description');
+            if (descBtn) {
+                descBtn.setAttribute('data-action-parameters', thumbnailElement.dataset.id);
+            }
+
+            // Hidden image id
+            let idField = galleryElement.querySelector('.osy-bcl4-gallery-image-id');
+            if (idField) {
+                idField.value = thumbnailElement.dataset.id;
+            }
+
+            // Extra fields
             if (!fieldValues) {
                 return;
             }
-            for (fid in fieldValues) {                    
-                if (fid  !== 'id' && fid !== 'url') {
-                    let field = $('.modalImageViewer #' + fid, galleryElement);
+            for (let fid in fieldValues) {
+                if (fid !== 'id' && fid !== 'url') {
+                    let field = galleryElement.querySelector('.modalImageViewer #' + fid);
                     if (field) {
-                        $(field).val(fieldValues[fid]);
+                        field.value = fieldValues[fid];
                     }
-                }                    
-            }              
+                }
+            }
         });
     }
 };
